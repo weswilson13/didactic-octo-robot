bf614f0386ea44226ed015b043808bc712a086a2
Wes Wilson <poweredge.t320.server@gmail.com>
2023-11-24 14:19:01 -0500

    add departments

--- Update-ExcelFromSql.ps1
+++ Update-ExcelFromSql.ps1
@@ -13,23 +13,29 @@ param(
     ,
     [Parameter(Mandatory=$true)]
     [String]$Path
-    ,
-    [Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
-    [String]$WorksheetName
-    ,
-    [Parameter(Mandatory=$false)]
-    [String]$Query = "Select * from hm_accounts"
-    ,
-    [Parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
-    [String]$Title
+    # ,
+    # [Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+    # [String]$WorksheetName
+    # ,
+    # [Parameter(Mandatory=$false)]
+    # [String]$Query = "Select * from hm_accounts"
+    # ,
+    # [Parameter(Mandatory=$true, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+    # [String]$Title
 )
 
 $PSBoundParameters.Remove("ServerInstance")
 $PSBoundParameters.Add("Connection", $ServerInstance)
 $PSboundParameters.Add("MsSqlServer", $true)
 $PSboundParameters.Add("RangeName", "Data")
-$PSboundParameters.Add("SQL", $Query)
+# $PSboundParameters.Add("SQL", $Query)
 
 $PSBoundParameters | Out-String | Write-Verbose
 
-Send-SQLDataToExcel @PSBoundParameters
+$departments = (Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $Database -Query "Select distinct Name from [HumanResources].[Department]").Name
+$departments | Out-String | Write-Verbose
+
+foreach ($dept in $departments) {
+    $PSBoundParameters["SQL"]="select * from [HumanResources].[vEmployeeDepartment] where department='$dept'"
+    Send-SQLDataToExcel @PSBoundParameters -WorksheetName $dept -Title "Monthly Asset Inventory ($dept) $(Get-Date -Format 'MMMM yyyy')"
+}
