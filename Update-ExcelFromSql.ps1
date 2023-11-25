6d212c24cc294d58c7eb02aa07b6f226a219db01
Wes Wilson <poweredge.t320.server@gmail.com>
2023-11-25 08:58:47 -0500

    added feedback. refactored.

--- Update-ExcelFromSql.ps1
+++ Update-ExcelFromSql.ps1
@@ -1,26 +1,45 @@
 using Module ImportExcel
 
-New-PSDrive -Name T -Root \\raspberrypi4-1\nas04 -PSProvider Filesystem -ErrorAction SilentlyContinue | Out-Null
+$dateString = Get-Date -Format 'MMMM yyyy'
 
-$serverInstance = "SQ02\MYSQLSERVER,9999"
-$database = "AdventureWorks2019"
-$path = "T:\November2023.xlsx"
+#region requires modification
+    #region create drive mappings
+        New-PSDrive -Name T -Root "\\raspberrypi4-1\nas04" -PSProvider Filesystem -Persist -ErrorAction SilentlyContinue | Out-Null
+    #endregion
+    $serverInstance = "SQ02\MYSQLSERVER,9999"
+    $database = "AdventureWorks2019"
+    $templateWorkbookName = "T:\template.xlsx"
+    $templateSheetName = "Sheet1"
+
+    # queries
+    $departmentsQuery = "Select distinct Name from [HumanResources].[Department]" # return the departments/divisions
+    
+#endregion
 
 $parameters = @{
     Connection = $serverInstance
     MsSqlServer = $true
     Database = $database
-    Path = $path
+    Path = "T:\$dateString.xlsx"
     RangeName = "Data"
+    KillExcel = $true
+    AutoSize = $true
 }
 
-$parameters | Out-String | Write-Verbose
+Write-Host "Core Parameters...`r`n$($parameters | Out-String)" -ForegroundColor Cyan
 
-$departments = (Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $Database -Query "Select distinct Name from [HumanResources].[Department]").Name
-$departments | Out-String | Write-Verbose
+$departments = (Invoke-Sqlcmd -ServerInstance $ServerInstance -Database $Database -Query $departmentsQuery).Name
+
+Write-Host "Departments...`r`n`n$($departments | Out-String)" -ForegroundColor Yellow
 
 foreach ($dept in $departments) {
-    Copy-ExcelWorksheet -SourceWorkbook \\raspberrypi4-1\nas04\template.xlsx -SourceWorksheet "Sheet1" -DestinationWorkbook $path -DestinationWorksheet $dept
-    $parameters["SQL"] = "select * from [HumanResources].[vEmployeeDepartment] where department='$dept'"
-    Send-SQLDataToExcel @parameters -WorksheetName $dept -Title "Monthly Asset Inventory ($dept) $(Get-Date -Format 'MMMM yyyy')" -KillExcel -AutoSize -
+    Write-Host "Creating the $dept Sheet..." 
+    Copy-ExcelWorksheet -SourceWorkbook $templateWorkbookName -SourceWorksheet $templateSheetName -DestinationWorkbook $parameters.path -DestinationWorksheet $dept
+    $parameters["SQL"] = "select * from [HumanResources].[vEmployeeDepartment] where department='$dept'" # return the data to be exported to each sheet
+    $parameters["WorksheetName"] = $dept
+    $parameters["Title"] = "Monthly Asset Inventory ($dept) $dateString"
+    Send-SQLDataToExcel @parameters
+    Write-Host "Done."
 }
+
+Remove-PSDrive -Name T -PSProvider FileSystem -Force
