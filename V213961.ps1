b7dfb08246d2202f320fd13b4180e82e3d5a8caa
Wes Wilson <poweredge.t320.server@gmail.com>
2024-01-26 09:48:54 -0500

    ready to deploy

--- V213961.ps1
+++ V213961.ps1
@@ -1,6 +1,11 @@
 $Instance = "SQ02,9999"
+[bool]$checkFailed = $false
+$failedProtocols = @()
+$protocols = @()
 
-#region From Scan-SqlServer2016Instance_Checks.psm1\Get-V213961 
+$approvedProtocols ='Shared Memory'
+
+#region Adapted from Scan-SqlServer2016Instance_Checks.psm1\Get-V213961 
     $ThisInst = $(Invoke-SqlCmd -ServerInstance $Instance -Query "
         SELECT CASE
                 WHEN SERVERPROPERTY ('InstanceName') IS NULL THEN 'MSSQLSERVER'
@@ -12,11 +17,30 @@ $Instance = "SQ02,9999"
     $RegSQLVal = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\Instance Names\SQL" -Name $ThisInst
 
     # Get SQL connection settings
-    $regprotocolNamed_Pipes = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$RegSQLVal\MSSQLServer\SuperSocketNetLib\Np" -Name "Enabled"
-    $regprotocolShared_Memory = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$RegSQLVal\MSSQLServer\SuperSocketNetLib\Sm" -Name "Enabled"
-    $regprotocolTCP = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$RegSQLVal\MSSQLServer\SuperSocketNetLib\Tcp" -Name "Enabled"
-    $regprotocolVIA = Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$RegSQLVal\MSSQLServer\SuperSocketNetLib\Via" -Name "Enabled"
+    $protocols += [PSCustomObject]@{
+        Protocol='Named Pipes'
+        Value=Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$RegSQLVal\MSSQLServer\SuperSocketNetLib\Np" -Name "Enabled"
+    }
+    $protocols += [PSCustomObject]@{
+        Protocol='Shared Memory'
+        Value=Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$RegSQLVal\MSSQLServer\SuperSocketNetLib\Sm" -Name "Enabled"
+    }
+    $protocols += [PSCustomObject]@{
+        Protocol='TCP/IP'
+        Value=Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$RegSQLVal\MSSQLServer\SuperSocketNetLib\Tcp" -Name "Enabled"
+    }
+    $protocols += [PSCustomObject]@{
+        Protocol='VIA'
+        Value=Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Microsoft\Microsoft SQL Server\$RegSQLVal\MSSQLServer\SuperSocketNetLib\Via" -Name "Enabled"
+    }
 #endregion
 
-$FindingDetails = Get-Variable regprotocol* | Select @{Name='Protocol'; Expression={$_.Name -replace 'regprotocol'}}, Value
-return $FindingDetails
+#$FindingDetails = Get-Variable regprotocol* | Select-Object @{Name='Protocol'; Expression={$_.Name -replace 'regprotocol'}},Value
+
+$protocols.Where({$_.Value -eq 1}) | Foreach-Object {
+    if ($_.Protocol -notin $approvedProtocols) {$checkFailed=$true; $failedProtocols+=$_.Protocol}
+}
+
+if ($checkFailed){"[CHECK FAILED]: The following protocol(s) are not documented: `n$($failedProtocols | Out-String)"; return $false}
+
+return $true
