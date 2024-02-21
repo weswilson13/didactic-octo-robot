097389ec1f241a4a455311f47f1028a38eb1ba3f
Wes Wilson <poweredge.t320.server@gmail.com>
2024-02-20 20:38:14 -0500

    add parameters, some defensive programming

--- Get-LatestBackupFile.ps1
+++ Get-LatestBackupFile.ps1
@@ -1,9 +1,16 @@
-$backupRepository = "C:\tools\test"
+Param(
+    [Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+    [System.IO.FileInfo]$BackupRepository = "C:\tools\test"
+    ,
+    [Parameter(Mandatory=$false, ValueFromPipeline=$false, ValueFromPipelineByPropertyName=$true)]
+    [string]$Format = "MMyyddHHmmss"
+)
 
-$files = Get-Childitem $backupRepository -File -Recurse
+$files = Get-Childitem $BackupRepository -File -Recurse 
 $files | Foreach-Object {
     $split=[System.IO.Path]::GetFileNameWithoutExtension($PSitem.Name) -split '_'
-    Add-Member -InputObject $PSItem -NotePropertyMembers @{SwitchName=$split[0];BackupDate=[datetime]::ParseExact("$($split[1]+$split[2])",'MMddyyHHmmss',$null)}
+    $arrayLength = $split.Length-1
+    Add-Member -InputObject $PSItem -NotePropertyMembers @{SwitchName=$split[0];BackupDate=[datetime]::ParseExact("$(-join $split[1..$arrayLength])",$Format,$null)}
 }
 
 $newestFiles = $files | Group-Object -Property SwitchName | Foreach-Object { $_.Group | Sort-Object BackupDate -Descending | Select-Object -First 1 }
