4e26964535a7ef15d95705cf60efa31cd199f022
Wes Wilson <wes_admin@mydomain.local>
2024-06-10 20:29:19 -0400

    psexec

--- uninstallMethod.ps1
+++ uninstallMethod.ps1
@@ -26,13 +26,29 @@ $scriptblock = {
     
     Get-InstalledSoftware -ComputerName $ComputerName -Software $software | Foreach-Object {
         if ((Read-Host "Uninstall $($PSItem.DisplayName)? (y/n)") -eq 'y') {
-            $uninstallCommand=switch -Regex ($PSItem.UninstallString) {
-                'msiexec' {$($PSItem -replace "/(I|X) *{","/X `"{" -replace "}$","}`""); break}
-                'C:\\' {"$($PSItem) --silent"; break}
+            switch -Regex ($PSItem.UninstallString) {
+                'msiexec' { 
+                    # $guid = ($PSItem | Select-String -Pattern '{.*}').Matches.Value 
+                    # $arg = "/X `"$guid`" /qn /norestart /v*l `"c:\tools\log.log`""
+                    $arg = "$PSItem /qn /L*V `"c:\tools\log.log`""
+                    write-host $arg
+                    $proc = Start-Process -Filepath \\OptimusPrime\Z\Microsoft\SysInternals\PsExec.exe -ArgumentList "\\$ComputerName -s", $arg -PassThru -Wait
+                    $proc.WaitForExit()  
+                    break
+                }
+                'C:\\' {
+                    # $exe = ($PSItem | Select-String -Pattern '".*"').Matches.Value.trim()
+                    # write-host $exe
+                    $arg = $PSItem.trim()
+                    write-host $arg
+                    # $arg += " --silent"
+                    $arg | Out-String | Write-Host
+                    $proc = Start-Process -Filepath \\OptimusPrime\Z\Microsoft\SysInternals\PsExec.exe -ArgumentList "\\$ComputerName -s", $arg -PassThru -Wait
+                    $proc.WaitForExit()
+                    break
+                }
                 {[string]::IsNullOrWhiteSpace($PSItem)} {Write-Host "No Uninstall String available."; break}
             }
-            Write-Host $uninstallCommand
-            Invoke-Command -ScriptBlock {cmd.exe /c "$uninstallCommand"} -ComputerName $ComputerName 
         }
     }
 }
