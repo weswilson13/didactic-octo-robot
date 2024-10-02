7e21e837b040f665861d03d9c34a9305268309a4
Wes Wilson <wes_admin@mydomain.local>
2024-10-02 06:52:19 -0400

    sols

---
+++ faroShuffle/Extensions.cs
@@ -0,0 +1,23 @@
+﻿// Extensions.cs
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqFaroShuffle
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> InterleaveSequenceWith<T>(this IEnumerable<T> first, IEnumerable<T> second)
+        {
+            var firstIter = first.GetEnumerator();
+            var secondIter = second.GetEnumerator();
+
+            while (firstIter.MoveNext() && secondIter.MoveNext())
+            {
+                yield return firstIter.Current;
+                yield return secondIter.Current;
+            }
+        }
+    }
+
+}
---
+++ faroShuffle/Program.cs
@@ -0,0 +1,59 @@
+﻿using LinqFaroShuffle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace faroShuffle;
+
+internal class Program {
+    // Program.cs
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Suits());
+        //var startingDeck = from s in Suits()
+        //                from r in Ranks()
+        //                select new { Suit = s, Rank = r };
+
+        var startingDeck = Suits().SelectMany(s => Ranks().Select(r => new { Suit = s, Rank = r }));
+
+        // Display each card that we've generated and placed in startingDeck in the console
+        foreach (var card in startingDeck)
+        {
+            Console.WriteLine(card);
+        }
+
+        var top = startingDeck.Take(26);
+        var bottom = startingDeck.Skip(26);
+        var shuffle = top.InterleaveSequenceWith(bottom);
+
+        foreach (var card in shuffle)
+        {
+            Console.WriteLine(card);
+        }
+    }
+
+    static IEnumerable<string> Suits() 
+    {
+        yield return "clubs";
+        yield return "hearts";
+        yield return "diamonds";
+        yield return "spades";
+    }
+
+    static IEnumerable<string> Ranks() 
+    {
+        yield return "two";
+        yield return "three";
+        yield return "four";
+        yield return "five";
+        yield return "six";
+        yield return "seven";
+        yield return "eight";
+        yield return "nine";
+        yield return "ten";
+        yield return "jack";
+        yield return "queen";
+        yield return "king";
+        yield return "ace";
+    }
+}
