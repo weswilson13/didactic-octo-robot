42659a95b85d2958472f2c164a4d69a9a16dde12
Wes W <wes.wilson013@gmail.com>
2024-10-27 08:00:35 -0400

    a

--- Install-MsiExeUpdate.ps1
+++ Install-MsiExeUpdate.ps1
@@ -35,6 +35,39 @@ param(
 
 )
 
+function Use-RunAs 
+{    
+    # Check if script is running as Administrator and if not elevate it
+    # Use Check Switch to check if admin 
+     
+    param([Switch]$Check) 
+     
+    $IsAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()` 
+        ).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator") 
+         
+    if ($Check) { return $IsAdmin }   
+      
+    if ($MyInvocation.ScriptName -ne "") 
+    {  
+        if (-not $IsAdmin)  
+          {  
+            try 
+            {  
+                $arg = "-file `"$($MyInvocation.ScriptName)`"" 
+                Start-Process "$psHome\powershell.exe" -Verb Runas -ArgumentList $arg -ErrorAction 'stop'  
+            } 
+            catch 
+            { 
+                Write-Warning "Error - Failed to restart script elevated"  
+                break               
+            } 
+            exit 
+        }  
+    }  
+} 
+
+Use-RunAs 
+
 $logpath = "$env:TEMP\Install-MsiExeUpdate_Log.log"
 $PSDefaultParameterValues["Out-File:FilePath"]=$logPath
 $PSDefaultParameterValues["Out-File:Append"]=$true
