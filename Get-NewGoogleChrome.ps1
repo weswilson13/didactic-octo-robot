52fef99a44e04f46f6ea23b5849526fe463ba589
Wes W <wes.wilson013@gmail.com>
2024-12-11 21:29:38 -0500

    switch to use .txt file for currentVersion. extensive commenting

--- Get-NewGoogleChrome.ps1
+++ Get-NewGoogleChrome.ps1
@@ -1,41 +1,44 @@
+<#
+.DESCRIPTION
+Script to check latest stable release of Google Chrome. If a newer version is available, 
+the enterprise .msi is downloaded, and tracking file may be updated. 
+
+#>
+
 [CmdletBinding()]
 param()
 
-# turn off progress bar for this powershell session - this makes web request download exponentially faster
-$ProgressPreference = 'SilentlyContinue'
+# load Forms Namespace to access the Messagebox classes
+Add-Type -AssemblyName System.Windows.Forms
 
-$outputFile = "$env:USERPROFILE\Downloads\googlechromestandaloneenterprise64.msi" 
+# turn off progress bar for this powershell session - this makes web request download exponentially faster
+$ProgressPreference = 'SilentlyContinue' 
+# $ProgressPreference = 'Continue' will turn on the progress bar 
+
+$versionsTxt = ".\versions.txt" # path to a text file containing the latest downloaded versions
+$outputFile = "$env:USERPROFILE\Downloads\googlechromestandaloneenterprise64.msi" # path to downloaded file
 
 # Get the latest stable Chrome version
 # This URL lists every? Google Chrome version in the stable channel, in a JSON format
+# Reference: https://developer.chrome.com/docs/versionhistory/guide/
 $versionsUrl = "https://versionhistory.googleapis.com/v1/chrome/platforms/win/channels/stable/versions"
 $versionsJson = Invoke-WebRequest -Uri $versionsUrl | Select-Object -ExpandProperty Content
 $versions = ConvertFrom-Json $versionsJson # convert the JSON to an array of objects
 $latestVersion = $versions.versions[0].version # select the first object, which should be the latest version
                                                # we could sort descending on Version to ensure we get the latest
 
-
 # Get the latest downloaded version, store it in a variable called $currentVersion
-
-#region replace this with whatever method supplies latest downloaded version ($currentVersion)
-
-# Example - if we are using a .txt file where the last line is the latest version
-# $currentVersion = (Get-Content versions.txt).split('\n')[-1]
-
-$installers = Get-ChildItem Z:\Chrome -File | ? { $_.Extension -eq '.msi' }
-$installers | ForEach-Object {
-    $productCode = Get-MSIProperty -Property ProductCode -Path $PSItem
-    $null = Add-Member -InputObject $PSItem -PassThru -NotePropertyMembers @{ChromeVersion = [version](Get-MSIProductInfo $ProductCode.Value).ProductVersion}
+if (!(Test-Path $versionsTxt)) {
+    New-Item -Path $versionsTxt -ItemType File
 }
-$currentInstaller = $installers | Sort-Object -Property ChromeVersion -Descending | Select-Object -First 1
-$currentVersion = $currentInstaller.ChromeVersion
-#endregion replace this code
+try { $currentVersion = (Get-Content $versionsTxt).split('\n')[-1] } # [-1] returns the last index in the array
+catch {}
 
-$chrome = @{
+$chrome = [ordered]@{
     CurrentVersion = [version]$currentVersion
     LatestVersion = [version]$latestVersion
     Action = switch ([version]$currentVersion -lt [version]$latestVersion) {
-        $true { 'Download '}
+        $true { 'Download'}
         $false { 'Ignore' }
         default { 'Error' }
     }
@@ -43,17 +46,24 @@ $chrome = @{
 
 $chrome | Out-String | Write-Verbose
 
-$webRequestParams = @{
+# use a hashtable for readability
+$webRequestParams = @{ # this is the URL for the .msi download and the download location defined above
     Uri = "https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi"
     OutFile = $outputFile
 }
-switch ($chrome.Action) {
+
+switch ($chrome.Action) { # take appropriate action, write output to console
     'Download' { 
-        try { Invoke-WebRequest @webRequestParams; Write-Host "Downloaded a new version!" -ForegroundColor Green } 
-        catch { $error[0] }
-        end { break } 
+        try { 
+            Invoke-WebRequest @webRequestParams; 
+            Write-Host "Downloaded a new version!" -ForegroundColor Green
+            $ans=[System.Windows.Forms.MessageBox]::Show("Append this version to 'versions.txt'?", "Update File?",`
+                [System.Windows.Forms.MessageBoxButtons]::YesNo, [System.Windows.Forms.MessageBoxIcon]::Question)
+            if ($ans -eq 'Yes') { $latestVersion.ToString() | Out-File $versionsTxt -Append}
+            break } 
+        catch { throw $error[0] } 
     }
-    'Ignore' { Write-Host "Nothing new to pull down"; break }
+    'Ignore' { Write-Host "Nothing new to pull down" -ForegroundColor Yellow; break }
     'Error' { Write-Host "Something went wrong." }
 }
 
