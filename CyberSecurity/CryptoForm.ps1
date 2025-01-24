63a278f5d0a760985ed5d5cc1f11f893a5fd81e1
Wes Wilson <wes_admin@mydomain.local>
2025-01-24 14:47:36 -0500

    add scope level to _rsa and _cspp variables. codesign

--- CyberSecurity/CryptoForm.ps1
+++ CyberSecurity/CryptoForm.ps1
@@ -32,7 +32,7 @@ function EncryptFile {
     # Use RSACryptoServiceProvider to encrypt the AES key.
     # rsa is previously instantiated:
     #    $rsa = [RSACryptoServiceProvider]::new(cspp)
-    [byte[]]$keyEncrypted = $_rsa.Encrypt($aes.Key, $false)
+    [byte[]]$keyEncrypted = $script:_rsa.Encrypt($aes.Key, $false)
 
     # Create byte arrays to contain the length values of the key and IV.
     $lKey = $keyEncrypted.Length
@@ -101,7 +101,7 @@ function DecryptFile {
     param(
         [System.IO.FileInfo]$File
     )
-
+    
     # Create instance of Aes for symmetric decryption of the data.
     $aes = [System.Security.Cryptography.Aes]::Create()
 
@@ -122,27 +122,27 @@ function DecryptFile {
     $inFs.Read($lenIV, 0, 3)
 
     # Convert the lengths to integer values.
-    [int]$lenK = [System.BitConverter]::ToInt32($lenK, 0)
-    [int]$lenIV = [System.BitConverter]::ToInt32($lenIV, 0)
+    [int]$_lenK = [System.BitConverter]::ToInt32($lenK, 0)
+    [int]$_lenIV = [System.BitConverter]::ToInt32($lenIV, 0)
 
     # Determine the start position of the cipher text (startC) and its length(lenC).
-    [int]$startC = $lenK + $lenIV + 8
+    [int]$startC = $_lenK + $_lenIV + 8
     $lenC = [int]$inFs.Length - $startC
 
     # Create the byte arrays for the encrypted Aes key, the IV, and the cipher text.
-    [byte[]] $keyEncrypted = New-Object byte[] $lenK
-    [byte[]] $iv = New-Object byte[] $lenIV
+    [byte[]] $keyEncrypted = New-Object byte[] $_lenK
+    [byte[]] $iv = New-Object byte[] $_lenIV
 
     # Extract the key and IV starting from index 8 after the length values.
     $inFs.Seek(8, [System.IO.SeekOrigin]::Begin)
-    $inFs.Read($keyEncrypted, 0, $lenK)
-    $inFs.Seek(8 + $lenK, [System.IO.SeekOrigin]::Begin)
-    $inFs.Read($iv, 0, $lenIV)
+    $inFs.Read($keyEncrypted, 0, $_lenK)
+    $inFs.Seek(8 + $_lenK, [System.IO.SeekOrigin]::Begin)
+    $inFs.Read($iv, 0, $_lenIV)
 
     [System.IO.Directory]::CreateDirectory($decrFolder)
 
     # Use RSACryptoServiceProvider to decrypt the AES key.
-    [byte[]]$keyDecrypted = $_rsa.Decrypt($keyEncrypted, $false)
+    [byte[]]$keyDecrypted = $script:_rsa.Decrypt($keyEncrypted, $false)
 
     # Decrypt the key.
     $transform = $aes.CreateDecryptor($keyDecrypted, $iv);
@@ -185,15 +185,15 @@ function buttonCreateAsmKeys_Click([psobject]$sender, [System.EventArgs]$e) {
     #>
 
     # Stores a key pair in the key container.
-    $_cspp.KeyContainerName = $keyName;
-    $_rsa = [System.Security.Cryptography.RSACryptoServiceProvider]::new($_cspp)
-    $_rsa.PersistKeyInCsp = $true
+    $script:_cspp.KeyContainerName = $keyName
+    $script:_rsa = [System.Security.Cryptography.RSACryptoServiceProvider]::new($script:_cspp)
+    $script:_rsa.PersistKeyInCsp = $true
 
-    if ($_rsa.PublicOnly) {
-        $label1.Text = "Key: $($_cspp.KeyContainerName) - Public Only"
+    if ($script:_rsa.PublicOnly) {
+        $label1.Text = "Key: $($script:_cspp.KeyContainerName) - Public Only"
     }
     else {
-        $label1.Text = "Key: $($_cspp.KeyContainerName) - Full Key Pair"
+        $label1.Text = "Key: $($script:_cspp.KeyContainerName) - Full Key Pair"
     }
 }
 function buttonEncryptFile_Click([psobject]$sender, [System.EventArgs]$e) {
@@ -201,7 +201,7 @@ function buttonEncryptFile_Click([psobject]$sender, [System.EventArgs]$e) {
         .SYNOPSIS
         Click event handler for the Encrypt File button (buttonEncryptFile_Click)
     #>
-    if ($_rsa -eq $null) {
+    if ($script:_rsa -eq $null) {
         [System.Windows.Forms.MessageBox]::Show("Key not set.")
     } 
     else {
@@ -223,7 +223,7 @@ function buttonDecryptFile_Click([psobject]$sender, [System.EventArgs]$e) {
         .SYNOPSIS
         Click event handler for the Decrypt File button
     #>
-    if ($_rsa -eq $null) {
+    if ($script:_rsa -eq $null) {
         [System.Windows.Forms.MessageBox]::Show("Key not set.")
     }
     else {
@@ -255,7 +255,7 @@ function buttonExportPublicKey_Click([psobject]$sender, [System.EventArgs]$e) {
     # Save the public key created by the RSA to a file. Caution, persisting the key to a file is a security risk.
     [System.IO.Directory]::CreateDirectory($encrFolder)
     $sw = [System.IO.StreamWriter]::new($pubKeyFile, $false)
-    $sw.Write($_rsa.ToXmlString($false))
+    $sw.Write($script:_rsa.ToXmlString($false))
 
     # clean up
     $sw.Close()
@@ -272,18 +272,18 @@ function buttonImportPublicKey_Click([psobject]$sender, [System.EventArgs]$e) {
     #>
     
     $sr = [System.IO.StreamReader]::new($pubKeyFile)
-    $_cspp.KeyContainerName = $keyName
-    $_rsa = [System.Security.Cryptography.RSACryptoServiceProvider]::new($_cspp)
+    $script:_cspp.KeyContainerName = $keyName
+    $script:_rsa = [System.Security.Cryptography.RSACryptoServiceProvider]::new($script:_cspp)
 
     $keytxt = $sr.ReadToEnd()
-    $_rsa.FromXmlString($keytxt)
-    $_rsa.PersistKeyInCsp = $true
+    $script:_rsa.FromXmlString($keytxt)
+    $script:_rsa.PersistKeyInCsp = $true
 
-    if ($_rsa.PublicOnly) {
-        $label1.Text = "Key: $($_cspp.KeyContainerName) - Public Only"
+    if ($script:_rsa.PublicOnly) {
+        $label1.Text = "Key: $($script:_cspp.KeyContainerName) - Public Only"
     }
     else { 
-        "Key: $($_cspp.KeyContainerName) - Full Key Pair" 
+        $label1.Text = "Key: $($script:_cspp.KeyContainerName) - Full Key Pair" 
     }
 }
 function buttonGetPrivateKey_Click([psobject]$sender, [System.EventArgs]$e) {
@@ -297,15 +297,15 @@ function buttonGetPrivateKey_Click([psobject]$sender, [System.EventArgs]$e) {
 
         This task simulates the scenario of Alice using her private key to decrypt files encrypted by Bob.
     #>
-    $_cspp.KeyContainerName = $keyName
-    $_rsa = [System.Security.Cryptography.RSACryptoServiceProvider]::new($_cspp)
-    $_rsa.PersistKeyInCsp = $true
+    $script:_cspp.KeyContainerName = $keyName
+    $script:_rsa = [System.Security.Cryptography.RSACryptoServiceProvider]::new($script:_cspp)
+    $script:_rsa.PersistKeyInCsp = $true
 
-    if ($_rsa.PublicOnly) {
-        $label1.Text = "Key: $($_cspp.KeyContainerName) - Public Only"
+    if ($script:_rsa.PublicOnly) {
+        $label1.Text = "Key: $($script:_cspp.KeyContainerName) - Public Only"
     }
     else { 
-         $label1.Text = "Key: $($_cspp.KeyContainerName) - Full Key Pair" 
+         $label1.Text = "Key: $($script:_cspp.KeyContainerName) - Full Key Pair" 
     }
 }
 
@@ -333,14 +333,15 @@ $AppSettings=[System.Configuration.ConfigurationManager]::AppSettings
     $decrFolder = $AppSettings["DecryptFolder"]
 
     # Declare CspParameters and RsaCryptoServiceProvider objects with global scope of your Form class.
-    $_cspp = [System.Security.Cryptography.CspParameters]::new()
-    $_rsa = [System.Security.Cryptography.RSACryptoServiceProvider]::new()
-
+    New-Variable -Name _cspp -Value ([System.Security.Cryptography.CspParameters]::new()) -Option ReadOnly -Scope Global -Force
+    [System.Security.Cryptography.RSACryptoServiceProvider]$global:_rsa = [System.Security.Cryptography.RSACryptoServiceProvider]::new($script:_cspp)
+    $script:_rsa.PersistKeyInCsp = $true
+    
     # Public key file
-    $pubKeyFile = "$encrFolder\rsaPublicKey.txt";
+    $pubKeyFile = "$encrFolder\rsaPublicKey.txt"
 
     # Key container name for private/public key value pair.
-    $keyName = "Key01";
+    $keyName = "Key01"
 
 #endregion DECLARE GLOBAL OBJECTS
 
@@ -451,3 +452,156 @@ $form.Add_FormClosed({
 
 $form.Controls.AddRange(@($tableLayoutPanel))
 $form.ShowDialog()
+
+# SIG # Begin signature block
+# MIIb+QYJKoZIhvcNAQcCoIIb6jCCG+YCAQExDzANBglghkgBZQMEAgEFADB5Bgor
+# BgEEAYI3AgEEoGswaTA0BgorBgEEAYI3AgEeMCYCAwEAAAQQH8w7YFlLCE63JNLG
+# KX7zUQIBAAIBAAIBAAIBAAIBADAxMA0GCWCGSAFlAwQCAQUABCBBgMhF2598pDEz
+# jGAdGq04ra5Zm07YXllkcOsNDXFRPKCCFkIwggM7MIICI6ADAgECAhA2a84lByWj
+# mkYPfn9MTwxLMA0GCSqGSIb3DQEBCwUAMCMxITAfBgNVBAMMGHdlc19hZG1pbkBt
+# eWRvbWFpbi5sb2NhbDAeFw0yNDExMjQxNTE4NDFaFw0yNTExMjQxNTM4NDFaMCMx
+# ITAfBgNVBAMMGHdlc19hZG1pbkBteWRvbWFpbi5sb2NhbDCCASIwDQYJKoZIhvcN
+# AQEBBQADggEPADCCAQoCggEBAM8CN/dDl5ke/jDl/zWjZ86fzh2Cg6+fxF073Snu
+# kQCwz9QTNKFOFksmlVL/OD5Aqsyt5PJk3LAgTT0rypEL4DwVoNNcjK+H2JVaSzD0
+# S6OQYOfIJTYrGhFbwhvjkNHzyhx6u43F9eVchqtKY+uO30IQjXEi+05HgdU07+nl
+# lqcnkmxn6hyVsRqynSz6dMZcDJhtEfNw0Cq4PlbjxAYomS/OjnXjkd0L5WDeJHx1
+# wwunZBxRk/tAFRFmJvjejp13OtOYooywruB3OBfrETSl7e91VE6INRTxGMomVb0e
+# 6CBqVwkDVa2KWAiNbOevmUSvPM0y2q8jMtaIr7A4+TVPfhUCAwEAAaNrMGkwDgYD
+# VR0PAQH/BAQDAgeAMBMGA1UdJQQMMAoGCCsGAQUFBwMDMCMGA1UdEQQcMBqCGHdl
+# c19hZG1pbkBteWRvbWFpbi5sb2NhbDAdBgNVHQ4EFgQU66n1ytkczGZ1PZnl6wKm
+# 736Ax2kwDQYJKoZIhvcNAQELBQADggEBAC6Qq+qq8XhlcWGDfI1HJx5gMoBjW4UO
+# pUkP4u9O4zSnLKe6jYR4gXl1m0c4+0ToQLfYszwUCfm2DBLE5ceYJhsG1AFjLk+6
+# HPZ8ZZoF0p+MgYzhVm/irv7gVnt4zOf0ZuFlfdeqcl/4mYdumpfQ0jmWJQlVGFOA
+# K/RiAoc3MdJZ1T/4iRTNdB68AWcPftlZKv5FofHVm0gNPsydALkITbTKKfaEUKCq
+# 7H9mf7Z+XiQCFnBW6tfx6ijlLn4UMl/2w7xnJgJ0rxxgajfIqSk5uA0vMZAdp6cZ
+# Y4nNGMaEhFzyU/t4w/pjpGUoEyD/v0oH63t3y2ReqPKAtvSJQkN7FBgwggWNMIIE
+# daADAgECAhAOmxiO+dAt5+/bUOIIQBhaMA0GCSqGSIb3DQEBDAUAMGUxCzAJBgNV
+# BAYTAlVTMRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdp
+# Y2VydC5jb20xJDAiBgNVBAMTG0RpZ2lDZXJ0IEFzc3VyZWQgSUQgUm9vdCBDQTAe
+# Fw0yMjA4MDEwMDAwMDBaFw0zMTExMDkyMzU5NTlaMGIxCzAJBgNVBAYTAlVTMRUw
+# EwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5jb20x
+# ITAfBgNVBAMTGERpZ2lDZXJ0IFRydXN0ZWQgUm9vdCBHNDCCAiIwDQYJKoZIhvcN
+# AQEBBQADggIPADCCAgoCggIBAL/mkHNo3rvkXUo8MCIwaTPswqclLskhPfKK2FnC
+# 4SmnPVirdprNrnsbhA3EMB/zG6Q4FutWxpdtHauyefLKEdLkX9YFPFIPUh/GnhWl
+# fr6fqVcWWVVyr2iTcMKyunWZanMylNEQRBAu34LzB4TmdDttceItDBvuINXJIB1j
+# KS3O7F5OyJP4IWGbNOsFxl7sWxq868nPzaw0QF+xembud8hIqGZXV59UWI4MK7dP
+# pzDZVu7Ke13jrclPXuU15zHL2pNe3I6PgNq2kZhAkHnDeMe2scS1ahg4AxCN2NQ3
+# pC4FfYj1gj4QkXCrVYJBMtfbBHMqbpEBfCFM1LyuGwN1XXhm2ToxRJozQL8I11pJ
+# pMLmqaBn3aQnvKFPObURWBf3JFxGj2T3wWmIdph2PVldQnaHiZdpekjw4KISG2aa
+# dMreSx7nDmOu5tTvkpI6nj3cAORFJYm2mkQZK37AlLTSYW3rM9nF30sEAMx9HJXD
+# j/chsrIRt7t/8tWMcCxBYKqxYxhElRp2Yn72gLD76GSmM9GJB+G9t+ZDpBi4pncB
+# 4Q+UDCEdslQpJYls5Q5SUUd0viastkF13nqsX40/ybzTQRESW+UQUOsxxcpyFiIJ
+# 33xMdT9j7CFfxCBRa2+xq4aLT8LWRV+dIPyhHsXAj6KxfgommfXkaS+YHS312amy
+# HeUbAgMBAAGjggE6MIIBNjAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBTs1+OC
+# 0nFdZEzfLmc/57qYrhwPTzAfBgNVHSMEGDAWgBRF66Kv9JLLgjEtUYunpyGd823I
+# DzAOBgNVHQ8BAf8EBAMCAYYweQYIKwYBBQUHAQEEbTBrMCQGCCsGAQUFBzABhhho
+# dHRwOi8vb2NzcC5kaWdpY2VydC5jb20wQwYIKwYBBQUHMAKGN2h0dHA6Ly9jYWNl
+# cnRzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEFzc3VyZWRJRFJvb3RDQS5jcnQwRQYD
+# VR0fBD4wPDA6oDigNoY0aHR0cDovL2NybDMuZGlnaWNlcnQuY29tL0RpZ2lDZXJ0
+# QXNzdXJlZElEUm9vdENBLmNybDARBgNVHSAECjAIMAYGBFUdIAAwDQYJKoZIhvcN
+# AQEMBQADggEBAHCgv0NcVec4X6CjdBs9thbX979XB72arKGHLOyFXqkauyL4hxpp
+# VCLtpIh3bb0aFPQTSnovLbc47/T/gLn4offyct4kvFIDyE7QKt76LVbP+fT3rDB6
+# mouyXtTP0UNEm0Mh65ZyoUi0mcudT6cGAxN3J0TU53/oWajwvy8LpunyNDzs9wPH
+# h6jSTEAZNUZqaVSwuKFWjuyk1T3osdz9HNj0d1pcVIxv76FQPfx2CWiEn2/K2yCN
+# NWAcAgPLILCsWKAOQGPFmCLBsln1VWvPJ6tsds5vIy30fnFqI2si/xK4VC0nftg6
+# 2fC2h5b9W9FcrBjDTZ9ztwGpn1eqXijiuZQwggauMIIElqADAgECAhAHNje3JFR8
+# 2Ees/ShmKl5bMA0GCSqGSIb3DQEBCwUAMGIxCzAJBgNVBAYTAlVTMRUwEwYDVQQK
+# EwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5jb20xITAfBgNV
+# BAMTGERpZ2lDZXJ0IFRydXN0ZWQgUm9vdCBHNDAeFw0yMjAzMjMwMDAwMDBaFw0z
+# NzAzMjIyMzU5NTlaMGMxCzAJBgNVBAYTAlVTMRcwFQYDVQQKEw5EaWdpQ2VydCwg
+# SW5jLjE7MDkGA1UEAxMyRGlnaUNlcnQgVHJ1c3RlZCBHNCBSU0E0MDk2IFNIQTI1
+# NiBUaW1lU3RhbXBpbmcgQ0EwggIiMA0GCSqGSIb3DQEBAQUAA4ICDwAwggIKAoIC
+# AQDGhjUGSbPBPXJJUVXHJQPE8pE3qZdRodbSg9GeTKJtoLDMg/la9hGhRBVCX6SI
+# 82j6ffOciQt/nR+eDzMfUBMLJnOWbfhXqAJ9/UO0hNoR8XOxs+4rgISKIhjf69o9
+# xBd/qxkrPkLcZ47qUT3w1lbU5ygt69OxtXXnHwZljZQp09nsad/ZkIdGAHvbREGJ
+# 3HxqV3rwN3mfXazL6IRktFLydkf3YYMZ3V+0VAshaG43IbtArF+y3kp9zvU5Emfv
+# DqVjbOSmxR3NNg1c1eYbqMFkdECnwHLFuk4fsbVYTXn+149zk6wsOeKlSNbwsDET
+# qVcplicu9Yemj052FVUmcJgmf6AaRyBD40NjgHt1biclkJg6OBGz9vae5jtb7IHe
+# IhTZgirHkr+g3uM+onP65x9abJTyUpURK1h0QCirc0PO30qhHGs4xSnzyqqWc0Jo
+# n7ZGs506o9UD4L/wojzKQtwYSH8UNM/STKvvmz3+DrhkKvp1KCRB7UK/BZxmSVJQ
+# 9FHzNklNiyDSLFc1eSuo80VgvCONWPfcYd6T/jnA+bIwpUzX6ZhKWD7TA4j+s4/T
+# Xkt2ElGTyYwMO1uKIqjBJgj5FBASA31fI7tk42PgpuE+9sJ0sj8eCXbsq11GdeJg
+# o1gJASgADoRU7s7pXcheMBK9Rp6103a50g5rmQzSM7TNsQIDAQABo4IBXTCCAVkw
+# EgYDVR0TAQH/BAgwBgEB/wIBADAdBgNVHQ4EFgQUuhbZbU2FL3MpdpovdYxqII+e
+# yG8wHwYDVR0jBBgwFoAU7NfjgtJxXWRM3y5nP+e6mK4cD08wDgYDVR0PAQH/BAQD
+# AgGGMBMGA1UdJQQMMAoGCCsGAQUFBwMIMHcGCCsGAQUFBwEBBGswaTAkBggrBgEF
+# BQcwAYYYaHR0cDovL29jc3AuZGlnaWNlcnQuY29tMEEGCCsGAQUFBzAChjVodHRw
+# Oi8vY2FjZXJ0cy5kaWdpY2VydC5jb20vRGlnaUNlcnRUcnVzdGVkUm9vdEc0LmNy
+# dDBDBgNVHR8EPDA6MDigNqA0hjJodHRwOi8vY3JsMy5kaWdpY2VydC5jb20vRGln
+# aUNlcnRUcnVzdGVkUm9vdEc0LmNybDAgBgNVHSAEGTAXMAgGBmeBDAEEAjALBglg
+# hkgBhv1sBwEwDQYJKoZIhvcNAQELBQADggIBAH1ZjsCTtm+YqUQiAX5m1tghQuGw
+# GC4QTRPPMFPOvxj7x1Bd4ksp+3CKDaopafxpwc8dB+k+YMjYC+VcW9dth/qEICU0
+# MWfNthKWb8RQTGIdDAiCqBa9qVbPFXONASIlzpVpP0d3+3J0FNf/q0+KLHqrhc1D
+# X+1gtqpPkWaeLJ7giqzl/Yy8ZCaHbJK9nXzQcAp876i8dU+6WvepELJd6f8oVInw
+# 1YpxdmXazPByoyP6wCeCRK6ZJxurJB4mwbfeKuv2nrF5mYGjVoarCkXJ38SNoOeY
+# +/umnXKvxMfBwWpx2cYTgAnEtp/Nh4cku0+jSbl3ZpHxcpzpSwJSpzd+k1OsOx0I
+# SQ+UzTl63f8lY5knLD0/a6fxZsNBzU+2QJshIUDQtxMkzdwdeDrknq3lNHGS1yZr
+# 5Dhzq6YBT70/O3itTK37xJV77QpfMzmHQXh6OOmc4d0j/R0o08f56PGYX/sr2H7y
+# Rp11LB4nLCbbbxV7HhmLNriT1ObyF5lZynDwN7+YAN8gFk8n+2BnFqFmut1VwDop
+# hrCYoCvtlUG3OtUVmDG0YgkPCr2B2RP+v6TR81fZvAT6gt4y3wSJ8ADNXcL50CN/
+# AAvkdgIm2fBldkKmKYcJRyvmfxqkhQ/8mJb2VVQrH4D6wPIOK+XW+6kvRBVK5xMO
+# Hds3OBqhK/bt1nz8MIIGvDCCBKSgAwIBAgIQC65mvFq6f5WHxvnpBOMzBDANBgkq
+# hkiG9w0BAQsFADBjMQswCQYDVQQGEwJVUzEXMBUGA1UEChMORGlnaUNlcnQsIElu
+# Yy4xOzA5BgNVBAMTMkRpZ2lDZXJ0IFRydXN0ZWQgRzQgUlNBNDA5NiBTSEEyNTYg
+# VGltZVN0YW1waW5nIENBMB4XDTI0MDkyNjAwMDAwMFoXDTM1MTEyNTIzNTk1OVow
+# QjELMAkGA1UEBhMCVVMxETAPBgNVBAoTCERpZ2lDZXJ0MSAwHgYDVQQDExdEaWdp
+# Q2VydCBUaW1lc3RhbXAgMjAyNDCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoC
+# ggIBAL5qc5/2lSGrljC6W23mWaO16P2RHxjEiDtqmeOlwf0KMCBDEr4IxHRGd7+L
+# 660x5XltSVhhK64zi9CeC9B6lUdXM0s71EOcRe8+CEJp+3R2O8oo76EO7o5tLusl
+# xdr9Qq82aKcpA9O//X6QE+AcaU/byaCagLD/GLoUb35SfWHh43rOH3bpLEx7pZ7a
+# vVnpUVmPvkxT8c2a2yC0WMp8hMu60tZR0ChaV76Nhnj37DEYTX9ReNZ8hIOYe4jl
+# 7/r419CvEYVIrH6sN00yx49boUuumF9i2T8UuKGn9966fR5X6kgXj3o5WHhHVO+N
+# BikDO0mlUh902wS/Eeh8F/UFaRp1z5SnROHwSJ+QQRZ1fisD8UTVDSupWJNstVki
+# qLq+ISTdEjJKGjVfIcsgA4l9cbk8Smlzddh4EfvFrpVNnes4c16Jidj5XiPVdsn5
+# n10jxmGpxoMc6iPkoaDhi6JjHd5ibfdp5uzIXp4P0wXkgNs+CO/CacBqU0R4k+8h
+# 6gYldp4FCMgrXdKWfM4N0u25OEAuEa3JyidxW48jwBqIJqImd93NRxvd1aepSeNe
+# REXAu2xUDEW8aqzFQDYmr9ZONuc2MhTMizchNULpUEoA6Vva7b1XCB+1rxvbKmLq
+# fY/M/SdV6mwWTyeVy5Z/JkvMFpnQy5wR14GJcv6dQ4aEKOX5AgMBAAGjggGLMIIB
+# hzAOBgNVHQ8BAf8EBAMCB4AwDAYDVR0TAQH/BAIwADAWBgNVHSUBAf8EDDAKBggr
+# BgEFBQcDCDAgBgNVHSAEGTAXMAgGBmeBDAEEAjALBglghkgBhv1sBwEwHwYDVR0j
+# BBgwFoAUuhbZbU2FL3MpdpovdYxqII+eyG8wHQYDVR0OBBYEFJ9XLAN3DigVkGal
+# Y17uT5IfdqBbMFoGA1UdHwRTMFEwT6BNoEuGSWh0dHA6Ly9jcmwzLmRpZ2ljZXJ0
+# LmNvbS9EaWdpQ2VydFRydXN0ZWRHNFJTQTQwOTZTSEEyNTZUaW1lU3RhbXBpbmdD
+# QS5jcmwwgZAGCCsGAQUFBwEBBIGDMIGAMCQGCCsGAQUFBzABhhhodHRwOi8vb2Nz
+# cC5kaWdpY2VydC5jb20wWAYIKwYBBQUHMAKGTGh0dHA6Ly9jYWNlcnRzLmRpZ2lj
+# ZXJ0LmNvbS9EaWdpQ2VydFRydXN0ZWRHNFJTQTQwOTZTSEEyNTZUaW1lU3RhbXBp
+# bmdDQS5jcnQwDQYJKoZIhvcNAQELBQADggIBAD2tHh92mVvjOIQSR9lDkfYR25tO
+# CB3RKE/P09x7gUsmXqt40ouRl3lj+8QioVYq3igpwrPvBmZdrlWBb0HvqT00nFSX
+# gmUrDKNSQqGTdpjHsPy+LaalTW0qVjvUBhcHzBMutB6HzeledbDCzFzUy34VarPn
+# vIWrqVogK0qM8gJhh/+qDEAIdO/KkYesLyTVOoJ4eTq7gj9UFAL1UruJKlTnCVaM
+# 2UeUUW/8z3fvjxhN6hdT98Vr2FYlCS7Mbb4Hv5swO+aAXxWUm3WpByXtgVQxiBlT
+# VYzqfLDbe9PpBKDBfk+rabTFDZXoUke7zPgtd7/fvWTlCs30VAGEsshJmLbJ6ZbQ
+# /xll/HjO9JbNVekBv2Tgem+mLptR7yIrpaidRJXrI+UzB6vAlk/8a1u7cIqV0yef
+# 4uaZFORNekUgQHTqddmsPCEIYQP7xGxZBIhdmm4bhYsVA6G2WgNFYagLDBzpmk91
+# 04WQzYuVNsxyoVLObhx3RugaEGru+SojW4dHPoWrUhftNpFC5H7QEY7MhKRyrBe7
+# ucykW7eaCuWBsBb4HOKRFVDcrZgdwaSIqMDiCLg4D+TPVgKx2EgEdeoHNHT9l3ZD
+# BD+XgbF+23/zBjeCtxz+dL/9NWR6P2eZRi7zcEO1xwcdcqJsyz/JceENc2Sg8h3K
+# eFUCS7tpFk7CrDqkMYIFDTCCBQkCAQEwNzAjMSEwHwYDVQQDDBh3ZXNfYWRtaW5A
+# bXlkb21haW4ubG9jYWwCEDZrziUHJaOaRg9+f0xPDEswDQYJYIZIAWUDBAIBBQCg
+# gYQwGAYKKwYBBAGCNwIBDDEKMAigAoAAoQKAADAZBgkqhkiG9w0BCQMxDAYKKwYB
+# BAGCNwIBBDAcBgorBgEEAYI3AgELMQ4wDAYKKwYBBAGCNwIBFTAvBgkqhkiG9w0B
+# CQQxIgQglf62NyftcH3nt9WRQQtFtGTFwhy97m9zRpyBxVu4yo8wDQYJKoZIhvcN
+# AQEBBQAEggEAQ7I0EVwi/djz5Ll6e6IsjuYERn2c1iL5pzB2aD4UkIhQOor3wZip
+# 9X39fx04Jdr+Ro7WhaMm9IA2OIEVZCMhyD9S2ocqQY4QS9nrJSW0VLISIYhTJ9GC
+# z7GRDEyAWQWJnF7uKEMg6uab1Ijt1e/54UDusidJ5QJf3ZgMlfY7mMIxHuSl0Vcf
+# fh47ti6KD833J7cAeVDzXU47U1d9PV/1qTBTbv2XOdobXOdLd5AkFNXQYKiFKDDs
+# rewsFAU17Qn6YAG5ZdYi/74XbPhYm7gO63IETnzVcDSEdpOrRAUNLXNjGks+yRy1
+# c0EC8D28mhT0fyVElH4FmTuxRsIB2gFiraGCAyAwggMcBgkqhkiG9w0BCQYxggMN
+# MIIDCQIBATB3MGMxCzAJBgNVBAYTAlVTMRcwFQYDVQQKEw5EaWdpQ2VydCwgSW5j
+# LjE7MDkGA1UEAxMyRGlnaUNlcnQgVHJ1c3RlZCBHNCBSU0E0MDk2IFNIQTI1NiBU
+# aW1lU3RhbXBpbmcgQ0ECEAuuZrxaun+Vh8b56QTjMwQwDQYJYIZIAWUDBAIBBQCg
+# aTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0yNTAx
+# MjQxOTQ2MDRaMC8GCSqGSIb3DQEJBDEiBCDOd1+g3njCyrtWu3FYpZC3WecjhbaU
+# +1ohLaX8qmjTgzANBgkqhkiG9w0BAQEFAASCAgC8iBLgR/DV0RuBj8vi+ax3KHM6
+# mkglb+ILOSF5B0s9eDg9VHG40HgjR7K/+FpjbsgpHluXHWW9El3scSJdKL0G2o0e
+# lujf06Yo8g08wBr7+z+qFc8+tWK3KSaD37fSsq8X7210qlFLSTvuj7YOAQzorn8e
+# gvfoDXIi6d4YiHwKa+yBEcqlUd8Sxd3Xpd0YFSJym9jCSosVIH5YhckF0qTNyGi2
+# ppTIW0nYkDxFzyLS0cFoKKTzxMmpZkLsxZ4fYSThS7SHWKjW5gDMZltBwhgG8KZf
+# sk7wVlnAJvSxwq5QQxBc2lqeD0aZ7LniikPcg7YCxZ7m9WcfAc2UT+d0/S0K4Wzl
+# DVC0QiRhMpt5imar0sMKgP1LjzNRrCBUdAQCmiYutKAjDqAsU9e4IIlUvXzULtF8
+# LzsqIrmwGijVHHavONuTwv/0AELeY64Mdwkrgy9ZF4cKV2mfXZH6RxQbe4utIWUi
+# MLN/KGgOK/OVnjjm9ANNONdASmk/d2aBlTFuI/NLVJ13QJrB8XYoFE+PdChkziA4
+# z5zcyUSgVNuivplSIQXYKf40owOQrZ3Wi6kyBtFgOvRkvYbHfDPsTJ6XXim9hjCd
+# r52sLkCarQ/+2PvA5I5KH14lXDwm6bkW2Jt5HGKA2C948CQreelFZJT+w5yS8Z31
+# 49pYkb7kQtPumtt5wg==
+# SIG # End signature block
