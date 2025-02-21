ba7ebd4f4b5950fbe1242b661dc675f9252f3e39
Wes W <wes.wilson013@gmail.com>
2025-02-21 14:19:14 -0500

    updates

--- Printer/Get-PrinterStatusReport.ps1
+++ Printer/Get-PrinterStatusReport.ps1
@@ -9,12 +9,15 @@ function New-HtmlTable {
         [string]$TableClass
     )
 
-    $htmlPreContent = "<div class=""table-container"">"
-    $htmlPostContent = "</div>"
+    # $htmlPreContent = "<div class=""table-container"">"
+    # $htmlPostContent = "</div>"
 
-    $html = $InputObject | ConvertTo-Html -Fragment -PreContent $htmlPreContent -PostContent $htmlPostContent
-    $columns = ($InputObject | gm -MemberType Properties).Count
-    $tableTitle = New-TableTitle -Title $TableTitle -Columns $columns -Class bad
+    $html = $InputObject | ConvertTo-Html -Fragment #-PreContent $htmlPreContent -PostContent $htmlPostContent
+    $columns = $InputObject | Get-Member -MemberType Properties
+    if ($columns) {
+        $columns = $columns.Count
+    }
+    $tableTitle = New-TableTitle -Title $TableTitle -Columns $columns -Class $TableClass
     
     return $html.Replace('<table>', $tableTitle)
 }
@@ -46,6 +49,28 @@ function Get-PtrRecordName {
         throw "Could not parse $IPAddress. Make sure this is a valid IP Address."
     }
 }
+function Send-UpdateMessage {
+    # configure and send a mail message
+    $creds = switch -regex ($env:USERNAME) {
+        'wes_admin' { Import-Clixml -Path "Z:\Scripts\Credentials\poweredge.t320.server@gmail.com_cred.xml" }
+        'svc-SchTasks-RE' { Import-Clixml -Path "Z:\Scripts\Credentials\poweredge.t320.server@gmail.com_cred_svc-SchTasks_RE`$.xml" }
+    }
+    $credentials = New-Object -TypeName pscredential($creds.username, $creds.Password)
+    
+    $messageAttr = @{
+        SmtpServer = 'smtp.gmail.com'
+        Port = 587
+        UseSsl = $true
+        From = "poweredge.t320.server@gmail.com"
+        To = "wesley.s.wilson.ctr@gmail.com"
+        Subject = "Printer Config Errors"
+        Body = $htmlOutput
+        BodyAsHtml = $true
+        Credential = $credentials
+        ErrorAction = "Stop"
+    }
+    Send-MailMessage @messageAttr -
+}
 
 $csv = "Z:\Temp\PrinterStatus.csv"
 $printerStatus = Import-Csv $csv
@@ -56,10 +81,15 @@ $bad8021xUserName = @()
 $badNetworkIdHostName = @()
 $badDnsPtrRecord = @()
 $badDnsARecord = @()
+$bad8021x =@()
 
 # table titles
 $badDnsPtrRecordTitle = "Incorrect DNS 'PTR' Records"
 $badDnsARecordTitle = "Incorrect DNS 'A' Records"
+$badNetworkIdHostNameTitle = "Incorrect Network ID HostName"
+$bad8021xUserNameTitle = "Incorrect 802.1x UserName"
+$badCertificateCNTitle = "Incorrect SSL Certificate"
+$bad8021xTitle = "802.1x Not Enabled"
 
 foreach ($obj in $printerStatus) {
     # Check Printer Name against DNS Records
@@ -105,13 +135,24 @@ foreach ($obj in $printerStatus) {
             NetworkIDHostName = $obj.NetworkID_HostName
         }
     }
+
+    # check if 802.1x is enabled
+    if ($obj.'802.1x_Enabled' -eq 'FALSE') {
+        $bad8021x = [pscustomobject]@{
+            PrinterName = $obj.PrinterName
+            '802.1x_Enabled' = $obj.'802.1x_Enabled'
+        }
+    }
 }
+
+$badPrinters = $($badDnsPtrRecord.PrinterName; $badDnsARecord.PrinterName; $badNetworkIdHostName.PrinterName; $bad8021xUserName.PrinterName; $badCertificateCN.PrinterName) | Sort-Object -Unique
+$goodPrinters = $printerStatus | Where-Object {$_.PrinterName -notin $badPrinters} | Sort-Object PrinterName -Unique
 
 $htmlOutput = @"
 <html>
 <head>
-    <meta charset=""UTF-8"">
-    <meta name=""viewport"" content=""width=device-width, initial-scale=1.0"">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Printer Status</title>
     <style>
         header {
@@ -140,10 +181,11 @@ $htmlOutput = @"
             width: 95%
         }
 
-        .table-container#GroupChanges {
+        .table-container#satPrinters {
             display: grid;
             grid-template-columns: repeat(1, auto);
             gap: 20px;
+            width: 95%
             /* width: 100%; */
         }
 
@@ -175,17 +217,43 @@ $htmlOutput = @"
     <header>
         <h1>Printer Status</h1>
     </header>
+    <div class="table-container">
 "@
 
 $badDnsPtrRecordHtml = New-HtmlTable -InputObject $badDnsPtrRecord -TableTitle $badDnsPtrRecordTitle -TableClass bad
-
 $badDnsARecordHtml = New-HtmlTable -InputObject $badDnsARecord -TableTitle $badDnsARecordTitle -TableClass bad
+$badNetworkIdHostNameHtml = New-HtmlTable -InputObject $badNetworkIdHostName -TableTitle $badNetworkIdHostNameTitle -TableClass bad
+$bad8021xUserNameHtml = New-HtmlTable -InputObject $bad8021xUserName -TableTitle $bad8021xUserNameTitle -TableClass bad
+$badCertificateCNHtml = New-HtmlTable -InputObject $badCertificateCN -TableTitle $badCertificateCNTitle -TableClass bad
+$bad8021xHtml = New-HtmlTable -InputObject $bad8021x -TableTitle $bad8021xTitle -TableClass bad
 
-$htmlOutput += $badDnsPtrRecordHtml
-$htmlOutput += "</br>"
-$htmlOutput += $badDnsARecordHtml 
+$goodPrintersHtml = New-HtmlTable -InputObject $goodPrinters -TableTitle "SAT Printers" -TableClass good
+
+foreach ($var in (Get-Variable -Name bad*)) {
+    if ($var.Name -notmatch 'Html|Title') {
+        if ($var.Value) { 
+            $_var = "$($var.Name)Html"
+            $htmlOutput += (Get-Variable -Name $_var).Value
+        }
+    }
+}
+
+$htmlOutput += 
+@"
+        </div>
+"@
+
+if ($htmlOutput) { Send-UpdateMessage }
+
+$htmlOutput += 
+@"
+        </br>
+        <div class="table-container" id="satPrinters">
+"@
+$htmlOutput += $goodPrintersHtml
 
 $htmlOutput += @"
+        </div>
     </body>
 </head>
 </html>
