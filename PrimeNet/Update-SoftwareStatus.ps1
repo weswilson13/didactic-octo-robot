7c06cdc59a6343c6ae2e9e0bfffe99e4fac3995c
weswilson13 <102468132+weswilson13@users.noreply.github.com>
2025-04-08 18:20:47 -0400

    Update Update-SoftwareStatus.ps1
    transition to SoftwareVersions.csv over individual text files

--- PrimeNet/Update-SoftwareStatus.ps1
+++ PrimeNet/Update-SoftwareStatus.ps1
@@ -1,70 +1,34 @@
 $folder = "$env:USERPROFILE\OneDrive\OneDrive - PrimeNet\SoftwareVersions"
 $template = Join-Path $folder "SoftwareStatusTemplate.html"
 $firmware = Join-Path $folder "firmware.txt"
+$softwareVersionsCsv = Join-Path $folder "SoftwareVersions.csv"
+$softwareVersions = Import-Csv $softwareVersionsCsv
 
 # add more applications here as necessary. update html template accordingly.
 $printerModels = "8500,609,551,577,578,652,506,612,6800,776,5700"
-$applications = 'adobe','edge','chrome','firefox','git','linqpad','nodejs','npp','powershell','vmware','defender','terminal','vscode','winscp'
-$vmwareProducts = 'esxi','vcenter','tools','horizon','vsan'
 
-$content = Get-Content $template
+$htmlContent = Get-Content $template
+$htmlContent = $htmlContent.Replace('[date]',(Get-Date -f 'MM/dd/yyyy HH:mm'))
 
-$content = $content.Replace('[date]',(Get-Date -f 'MM/dd/yyyy HH:mm'))
+foreach ($obj in $softwareVersions) {
+    $version,$fileDate,$newContent = $null
 
-# get all text files files
-$files = Get-ChildItem $folder | Where-Object {$_.Extension -eq '.txt'}
+    # add the updated date to the output so it is easier to see the last time the software was pulled
+    $app = $obj.id
+    $version = $obj.LatestVersion
+    if ($version) {
+        $_date = $obj.DateTime
+        if ($_date) {
+            #Write-Host "$app, $_date"
+            if ([datetime]$_date -gt [datetime]::Now.AddHours(-24)) { $_date = "<span class=`"new`">$_date<span>" }
+        }
+        else {
+            $_date = "???" 
+        }
 
-foreach ($app in $applications) {
-    $version,$fileDate = $null
-
-    # get the associated text file
-    $file = $files | Where-Object {$_.basename -match $app}
-
-    if (!$file) { # update html and move to next
-        Write-Host "No file for $app" -ForegroundColor Yellow
-        $content = $content.Replace("[$app]","")
-        continue 
-    } # end if
-
-    # add the file date to the output so it is easier to see the last time the software was pulled
-    $fileDate = $file.LastWriteTime
-    
-    Write-Host "$file, $fileDate"
-    if ([datetime]$fileDate -gt [datetime]::Now.AddHours(-24)) { $fileDate = "<span class=`"new`">$fileDate<span>" }
-
-    if ($app -ne 'vmware') { # vmware file needs to be handled differently
-
-        $version = Get-Content $file.FullName
-        $newContent = "<b>$version</b>  ($fileDate)"
-        $content = $content.Replace("[$app]",$newContent)
-
-    } # end if
-
-    else { # parse each VMWare line separately
-        $versions = Get-Content $file.FullName
-
-
-        foreach ($product in $vmwareProducts) { # loop through each VMWare product
-
-            $ver = $versions.Where({$_ -match $product})
-
-            $version,$_app = switch($product) {
-                'esxi'{ @([regex]::Match($ver, '\d Update \d\w' ).Value, 'vmware_esxi'); break }
-                'vcenter' { @([regex]::Match($ver, '\d Update \d\w' ).Value, 'vmware_vcenter'); break }
-                'horizon' { @([regex]::Match($ver, '\d+\.\d+' ).Value, 'vmware_horizon'); break }
-                'tools' { @([regex]::Match($ver, '(\d+\.?)+' ).Value, 'vmware_tools'); break }
-                'vsan' { @([regex]::Match($ver, '(\d+\.?)+' ).Value, 'vmware_vsan'); break }
-                default { @("", "vmware_$product") }
-            }# end switch
-
-            $_fileDate = $fileDate
-            $newContent = "<b>$version</b>  ($_fileDate)"
-            if ($version -eq "") { $newContent = "" }
-            $content = $content.Replace("[$_app]",$newContent)
-
-        } # end foreach
-
-    }# end else
+        $newContent = "<b>$version</b>  ($_date)"
+    }
+    $htmlContent = $htmlContent.Replace("[$app]",$newContent)
 
 } # end foreach
 
@@ -72,13 +36,13 @@ foreach ($app in $applications) {
 $firmwareFileInfo = Get-ItemProperty $firmware
 $firmwares = Get-Content $firmware
 
-$content = $content.Replace('[printer-date]',$firmwareFileInfo.LastWriteTime)
+$htmlContent = $htmlContent.Replace('[printer-date]',$firmwareFileInfo.LastWriteTime)
 
 foreach ($printer in $printerModels.Split(',')) { # loop through each printer model 
 
     $latestFirmware = $firmwares | Where-Object { $_ -match "M?$printer" } | Select-Object -Last 1
-    $content = $content -replace "\[M?$printer\]",$latestFirmware
+    $htmlContent = $htmlContent -replace "\[M?$printer\]",$latestFirmware
 
 } # end foreach
 
-$content | Out-File "$folder\SoftwareStatus.html"
+$htmlContent | Out-File "$folder\SoftwareStatus.html"
