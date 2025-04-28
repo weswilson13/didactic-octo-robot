b40837daa72e8b2553ffee43309a81ec3a379a23
weswilson13 <102468132+weswilson13@users.noreply.github.com>
2025-04-28 14:57:05 -0400

    Update Update-SoftwareStatus.ps1

--- PrimeNet/Update-SoftwareStatus.ps1
+++ PrimeNet/Update-SoftwareStatus.ps1
@@ -1,40 +1,50 @@
 function New-Html {
     param([switch]$Server)
-     
-    $html = ""
-    if ($Server.IsPresent) {
-        $collection = $serverSoftware
-    }
-    else {
-        $collection = $softwareVersions
-    }
-    foreach ($software in $collection) {
-        $aryLinks = @()
-        $img, $checked = $null
-        $softwareName = $software.SoftwareName
-        if ($software.LogoOnly -eq 1) { $softwareName = $null }
-
-        if ($software.LoginRequired -eq 1) { $checked = " checked" }
-
+    
+    function Get-Links {
         $links = $software.Link.split(',') 
         $linkTexts = $software.LinkText.Split(';')
-        $src = Get-ChildItem .\images | Where-Object { $_.Name -match $software.Id } | Select-Object -ExpandProperty Name
-        if ($src) { $img = "<img height=`"30px`" src=`".\images\$src`">" }
-        <#switch($software.Id) {
-            'teradici' { "<img style=`"background-color:blue`" height=`"30px`" src=`".\images\teradici.svg`">"; break }
-            'hpwja' { "<img height=`"30px`" src=`".\images\hpwja.jpg`">"; break }
-            'servicepro' { "<img height=`"30px`" src=`".\images\servicepro.png`">"; break }
-            'ssms' { "<img height=`"30px`" src=`".\images\ssms.png`">"; break }
-            'vscode' { "<img height=`"30px`" src=`".\images\vscode.png`">"; break }
-            default { $null }
-        }#>
 
         for ($i=0; $i -lt $links.Count; $i++) {
             $aryLinks += "<a href=`"$($links[$i])`" target=`"_blank`">$($linkTexts[$i])</a>"
         }
-        $strLinks = $aryLinks -join "<br>`r"
 
-        if ($Server.IsPresent) {
+        return $aryLinks -join "<br>`r"
+    }
+    
+    $html = ""
+
+    if ($Server.IsPresent) { # use data from ServerSoftware.csv
+        $collection = $serverSoftware
+    }
+    else { # use date from SoftwareVersions.csv
+        $collection = $softwareVersions
+    }
+
+    foreach ($software in $collection) { # loop over each line in the .csv
+        $aryLinks = @()
+        $img, $checked = $null
+
+        # get software title
+        $softwareName = $software.SoftwareName
+        if ($software.LogoOnly -eq 1) { # software logo contains the name, so don't add text
+            $softwareName = $null 
+        }
+
+        # get login required checkbox
+        if ($software.LoginRequired -eq 1) { # login required to get to downloads. Show this displayed as a checked checkbox
+            $checked = " checked" 
+        }
+
+        # set up img element
+        $src = Get-ChildItem $folder\images | Where-Object { $_.Name -match $software.Id } | Select-Object -ExpandProperty Name
+        if ($src) { $img = "<img height=`"30px`" src=`".\images\$src`">" }
+
+        # get the software link(s)
+        $strLinks = Get-Links
+
+        # create the HTML
+        if ($Server.IsPresent) { # server software table format
             $html += "<tr class=`"$($software.ServerModel.Replace(' ',''))`">
                         <td class=`"ServerApp`">{0}</td>
                         <td>{1}</td>
@@ -44,7 +54,7 @@ function New-Html {
                         </td>
                     </tr>" -f $software.ServerModel, $software.ProductType, $software.Id, $strLinks
         }
-        else {
+        else { # software versions table format
             $html += "<tr>
                         <td class=`"{3} App`">$img{0}</td>
                         <td id=`"{1}`" class=`"Version`">[{1}]</td>
@@ -56,7 +66,8 @@ function New-Html {
         }
     }
     
-    foreach ($obj in $collection) {
+    # update the datetime the software was updated
+    foreach ($obj in $collection) { # loop through each line of the .csv
         $version,$fileDate,$newContent = $null
     
         # add the updated date to the output so it is easier to see the last time the software was pulled
@@ -65,7 +76,7 @@ function New-Html {
         if ($version) {
             $_date = $obj.DateTime
             if ($_date) {
-                #Write-Host "$app, $_date"
+                Write-Host "$app, $_date"
                 if ([datetime]$_date -gt [datetime]::Now.AddHours(-24)) { $_date = "<span class=`"new`">$_date<span>" }
             }
             else {
@@ -81,7 +92,8 @@ function New-Html {
     return $html
 }
 
-$folder = "$env:USERPROFILE\OneDrive\OneDrive - PrimeNet\SoftwareVersions"
+$folder = Get-ChildItem "$env:USERPROFILE\OneDrive\OneDrive - PrimeNet" -Directory | 
+          Where-Object {$_.Name -match 'SoftwareVersions'} | Select-Object -ExpandProperty FullName
 $template = Join-Path $folder "SoftwareStatusTemplate.html"
 $firmware = Join-Path $folder "firmware.txt"
 $softwareVersionsCsv = Join-Path $folder "SoftwareVersions.csv"
@@ -109,6 +121,7 @@ $firmwares = Get-Content $firmware
 
 $htmlContent = $htmlContent.Replace('[printer-date]',$firmwareFileInfo.LastWriteTime)
 $html = ""
+
 foreach ($printer in $printerModels) { # loop through each printer model 
     
     $latestFirmware = $firmwares | Where-Object { $_ -match "M?$printer" } | Select-Object -Last 1
