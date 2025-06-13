a96f1934b0dcea5adc220f1ddd050768a26631ab
Wes Wilson <wes_admin@mydomain.local>
2025-06-13 12:05:58 -0400

    initial commit

---
+++ NetworkStatusManager/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace NetworkStatusManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.deviceNameDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.portDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.serviceDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.notesDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.activeDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.iDDataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.networkStatusBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.scriptLogsDataSet = new NetworkStatusManager.ScriptLogsDataSet();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.sectionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.keyDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.valueDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.scriptConfigBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.networkStatusTableAdapter = new NetworkStatusManager.ScriptLogsDataSetTableAdapters.NetworkStatusTableAdapter();
+            this.scriptConfigTableAdapter = new NetworkStatusManager.ScriptLogsDataSetTableAdapters.ScriptConfigTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.networkStatusBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scriptLogsDataSet)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scriptConfigBindingSource)).BeginInit();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.deviceNameDataGridViewTextBoxColumn1,
+            this.portDataGridViewTextBoxColumn1,
+            this.serviceDataGridViewTextBoxColumn1,
+            this.notesDataGridViewTextBoxColumn1,
+            this.activeDataGridViewTextBoxColumn1,
+            this.iDDataGridViewTextBoxColumn1});
+            this.dataGridView1.DataSource = this.networkStatusBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(470, 549);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.RowValidated += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowValidated);
+            // 
+            // deviceNameDataGridViewTextBoxColumn1
+            // 
+            this.deviceNameDataGridViewTextBoxColumn1.DataPropertyName = "DeviceName";
+            this.deviceNameDataGridViewTextBoxColumn1.HeaderText = "DeviceName";
+            this.deviceNameDataGridViewTextBoxColumn1.Name = "deviceNameDataGridViewTextBoxColumn1";
+            this.deviceNameDataGridViewTextBoxColumn1.ReadOnly = true;
+            this.deviceNameDataGridViewTextBoxColumn1.Width = 94;
+            // 
+            // portDataGridViewTextBoxColumn1
+            // 
+            this.portDataGridViewTextBoxColumn1.DataPropertyName = "Port";
+            this.portDataGridViewTextBoxColumn1.HeaderText = "Port";
+            this.portDataGridViewTextBoxColumn1.Name = "portDataGridViewTextBoxColumn1";
+            this.portDataGridViewTextBoxColumn1.ReadOnly = true;
+            this.portDataGridViewTextBoxColumn1.Width = 51;
+            // 
+            // serviceDataGridViewTextBoxColumn1
+            // 
+            this.serviceDataGridViewTextBoxColumn1.DataPropertyName = "Service";
+            this.serviceDataGridViewTextBoxColumn1.HeaderText = "Service";
+            this.serviceDataGridViewTextBoxColumn1.Name = "serviceDataGridViewTextBoxColumn1";
+            this.serviceDataGridViewTextBoxColumn1.ReadOnly = true;
+            this.serviceDataGridViewTextBoxColumn1.Width = 68;
+            // 
+            // notesDataGridViewTextBoxColumn1
+            // 
+            this.notesDataGridViewTextBoxColumn1.DataPropertyName = "Notes";
+            this.notesDataGridViewTextBoxColumn1.HeaderText = "Notes";
+            this.notesDataGridViewTextBoxColumn1.Name = "notesDataGridViewTextBoxColumn1";
+            this.notesDataGridViewTextBoxColumn1.ReadOnly = true;
+            this.notesDataGridViewTextBoxColumn1.Width = 60;
+            // 
+            // activeDataGridViewTextBoxColumn1
+            // 
+            this.activeDataGridViewTextBoxColumn1.DataPropertyName = "Active";
+            this.activeDataGridViewTextBoxColumn1.HeaderText = "Active";
+            this.activeDataGridViewTextBoxColumn1.Name = "activeDataGridViewTextBoxColumn1";
+            this.activeDataGridViewTextBoxColumn1.ReadOnly = true;
+            this.activeDataGridViewTextBoxColumn1.Width = 62;
+            // 
+            // iDDataGridViewTextBoxColumn1
+            // 
+            this.iDDataGridViewTextBoxColumn1.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn1.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn1.Name = "iDDataGridViewTextBoxColumn1";
+            this.iDDataGridViewTextBoxColumn1.ReadOnly = true;
+            this.iDDataGridViewTextBoxColumn1.Width = 43;
+            // 
+            // networkStatusBindingSource
+            // 
+            this.networkStatusBindingSource.DataMember = "NetworkStatus";
+            this.networkStatusBindingSource.DataSource = this.scriptLogsDataSet;
+            this.networkStatusBindingSource.Sort = "DeviceName";
+            this.networkStatusBindingSource.CurrentChanged += new System.EventHandler(this.bindingSource1_CurrentChanged);
+            // 
+            // scriptLogsDataSet
+            // 
+            this.scriptLogsDataSet.DataSetName = "ScriptLogsDataSet";
+            this.scriptLogsDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.tabControl1.Location = new System.Drawing.Point(0, 30);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(484, 581);
+            this.tabControl1.TabIndex = 2;
+            this.tabControl1.SelectedIndexChanged += new System.EventHandler(this.tabControl_SelectedIndexChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(476, 555);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Script Configuration";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoGenerateColumns = false;
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.sectionDataGridViewTextBoxColumn,
+            this.keyDataGridViewTextBoxColumn,
+            this.valueDataGridViewTextBoxColumn});
+            this.dataGridView2.DataSource = this.scriptConfigBindingSource;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.Size = new System.Drawing.Size(470, 549);
+            this.dataGridView2.TabIndex = 0;
+            this.dataGridView2.RowValidated += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_RowValidated);
+            // 
+            // sectionDataGridViewTextBoxColumn
+            // 
+            this.sectionDataGridViewTextBoxColumn.DataPropertyName = "Section";
+            this.sectionDataGridViewTextBoxColumn.HeaderText = "Section";
+            this.sectionDataGridViewTextBoxColumn.Name = "sectionDataGridViewTextBoxColumn";
+            this.sectionDataGridViewTextBoxColumn.ReadOnly = true;
+            this.sectionDataGridViewTextBoxColumn.Width = 68;
+            // 
+            // keyDataGridViewTextBoxColumn
+            // 
+            this.keyDataGridViewTextBoxColumn.DataPropertyName = "Key";
+            this.keyDataGridViewTextBoxColumn.HeaderText = "Key";
+            this.keyDataGridViewTextBoxColumn.Name = "keyDataGridViewTextBoxColumn";
+            this.keyDataGridViewTextBoxColumn.ReadOnly = true;
+            this.keyDataGridViewTextBoxColumn.Width = 50;
+            // 
+            // valueDataGridViewTextBoxColumn
+            // 
+            this.valueDataGridViewTextBoxColumn.DataPropertyName = "Value";
+            this.valueDataGridViewTextBoxColumn.HeaderText = "Value";
+            this.valueDataGridViewTextBoxColumn.Name = "valueDataGridViewTextBoxColumn";
+            this.valueDataGridViewTextBoxColumn.ReadOnly = true;
+            this.valueDataGridViewTextBoxColumn.Width = 59;
+            // 
+            // scriptConfigBindingSource
+            // 
+            this.scriptConfigBindingSource.DataMember = "ScriptConfig";
+            this.scriptConfigBindingSource.DataSource = this.scriptLogsDataSet;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(476, 555);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Network Devices & Services";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(391, 7);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(76, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Read Only";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // networkStatusTableAdapter
+            // 
+            this.networkStatusTableAdapter.ClearBeforeFill = true;
+            // 
+            // scriptConfigTableAdapter
+            // 
+            this.scriptConfigTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(484, 611);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NNPTC Script Configuration Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.networkStatusBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scriptLogsDataSet)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.scriptConfigBindingSource)).EndInit();
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource networkStatusBindingSource;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private ScriptLogsDataSet scriptLogsDataSet;
+        private ScriptLogsDataSetTableAdapters.NetworkStatusTableAdapter networkStatusTableAdapter;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private ScriptLogsDataSetTableAdapters.ScriptConfigTableAdapter scriptConfigTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn deviceNameDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn portDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn serviceDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn notesDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn activeDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn1;
+        private System.Windows.Forms.BindingSource scriptConfigBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sectionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn keyDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn valueDataGridViewTextBoxColumn;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ NetworkStatusManager/ScriptLogsDataSet.cs
@@ -0,0 +1,21 @@
+﻿namespace NetworkStatusManager
+{
+
+
+    partial class ScriptLogsDataSet
+    {
+        partial class ScriptConfigDataTable
+        {
+        }
+    }
+}
+
+namespace NetworkStatusManager.ScriptLogsDataSetTableAdapters
+{
+    partial class ScriptConfigTableAdapter
+    {
+    }
+
+    public partial class NetworkStatusTableAdapter {
+    }
+}
