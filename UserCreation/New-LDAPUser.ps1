882ce1fdcd00b6f186c4ee8261a96e5211a417fd
Wes W <wes.wilson013@gmail.com>
2025-09-27 11:09:38 -0400

    incorporate uac enum

--- UserCreation/New-LDAPUser.ps1
+++ UserCreation/New-LDAPUser.ps1
@@ -60,12 +60,14 @@ Function New-LDAPUser {
     $new.setpassword("P@ssw0rd")
     Write-Verbose "force change at next logon"
     $new.Put("pwdLastSet", 0)
+
     if ($Disable) {
         Write-Verbose "Disabling the account"
-        $new.put("userAccountControl", 546)
-    }
+        $uac = [useraccountcontrol]::PASSWD_NOTREQD + [useraccountcontrol]::NORMAL_ACCOUNT + [UserAccountControl]::ACCOUNTDISABLE
+        $new.put("userAccountControl", $uac)    }
     else {
-        $new.put("userAccountControl", 544)
+        $uac = [useraccountcontrol]::PASSWD_NOTREQD + [useraccountcontrol]::NORMAL_ACCOUNT
+        $new.put("userAccountControl", $uac)
     }
     Write-Verbose "committing changes"
     $new.setinfo()
@@ -75,88 +77,93 @@ Function New-LDAPUser {
     }
 } #end function
 
+function Get-UACAttributes ($UAC) {
+    foreach($enum in [useraccountcontrol].GetEnumNames()) {
+        if (($UAC -band [useraccountcontrol]::$enum) -ne 0) { 
+            $enum 
+        }
+    }
+}
 
-   # <summary>
-   # Flags that control the behavior of the user account.
-   # </summary>
-   [flags()]enum UserAccountControl
-   {
-      # The logon script is executed. 
-      SCRIPT = 1
+# Flags that control the behavior of the user account.
+[flags()]enum UserAccountControl
+{
+    # The logon script is executed. 
+    SCRIPT = 1
 
-      # The user account is disabled. 
-      ACCOUNTDISABLE = 2
+    # The user account is disabled. 
+    ACCOUNTDISABLE = 2
 
-      # The home directory is required. 
-      HOMEDIR_REQUIRED = 8
+    # The home directory is required. 
+    HOMEDIR_REQUIRED = 8
 
-      # The account is currently locked out. 
-      LOCKOUT = 16
+    # The account is currently locked out. 
+    LOCKOUT = 16
 
-      # No password is required. 
-      PASSWD_NOTREQD = 32
+    # No password is required. 
+    PASSWD_NOTREQD = 32
 
-      # The user cannot change the password. 
-      # <remarks>
-      # Note:  You cannot assign the permission settings of PASSWD_CANT_CHANGE by directly modifying the UserAccountControl attribute. 
-      # For more information and a code example that shows how to prevent a user from changing the password see User Cannot Change Password.
-      # </remarks>
-      PASSWD_CANT_CHANGE = 64
+    # The user cannot change the password. 
+    # Note:  You cannot assign the permission settings of PASSWD_CANT_CHANGE by directly modifying the UserAccountControl attribute. 
+    # For more information and a code example that shows how to prevent a user from changing the password see User Cannot Change Password.
+    PASSWD_CANT_CHANGE = 64
 
-      # The user can send an encrypted password. 
-      ENCRYPTED_TEXT_PASSWORD_ALLOWED = 128
+    # The user can send an encrypted password. 
+    ENCRYPTED_TEXT_PASSWORD_ALLOWED = 128
 
-      # This is an account for users whose primary account is in another domain. This account provides user access to this domain but not 
-      # to any domain that trusts this domain. Also known as a local user account. 
-      TEMP_DUPLICATE_ACCOUNT = 256
+    # This is an account for users whose primary account is in another domain. This account provides user access to this domain but not 
+    # to any domain that trusts this domain. Also known as a local user account. 
+    TEMP_DUPLICATE_ACCOUNT = 256
 
-      # This is a default account type that represents a typical user. 
-      NORMAL_ACCOUNT = 512
+    # This is a default account type that represents a typical user. 
+    NORMAL_ACCOUNT = 512
 
-      # This is a permit to trust account for a system domain that trusts other domains. 
-      INTERDOMAIN_TRUST_ACCOUNT = 2048
+    # This is a permit to trust account for a system domain that trusts other domains. 
+    INTERDOMAIN_TRUST_ACCOUNT = 2048
 
-      # This is a computer account for a computer that is a member of this domain. 
-      WORKSTATION_TRUST_ACCOUNT = 4096
+    # This is a computer account for a computer that is a member of this domain. 
+    WORKSTATION_TRUST_ACCOUNT = 4096
 
-      # This is a computer account for a system backup domain controller that is a member of this domain. 
-      SERVER_TRUST_ACCOUNT = 8192
+    # This is a computer account for a system backup domain controller that is a member of this domain. 
+    SERVER_TRUST_ACCOUNT = 8192
 
-      Unused1 = 16384
+    Unused1 = 16384
 
-      Unused2 = 32768
+    Unused2 = 32768
 
-      # The password for this account will never expire. 
-      DONT_EXPIRE_PASSWD = 65536
+    # The password for this account will never expire. 
+    DONT_EXPIRE_PASSWD = 65536
 
-      # This is an MNS logon account. 
-      MNS_LOGON_ACCOUNT = 131072
+    # This is an MNS logon account. 
+    MNS_LOGON_ACCOUNT = 131072
 
-      # The user must log on using a smart card. 
-      SMARTCARD_REQUIRED = 262144
+    # The user must log on using a smart card. 
+    SMARTCARD_REQUIRED = 262144
 
-      # The service account (user or computer account) under which a service runs is trusted for Kerberos delegation. Any such service 
-      # can impersonate a client requesting the service. 
-      TRUSTED_FOR_DELEGATION = 524288
+    # The service account (user or computer account) under which a service runs is trusted for Kerberos delegation. Any such service 
+    # can impersonate a client requesting the service. 
+    TRUSTED_FOR_DELEGATION = 524288
 
-      # The security context of the user will not be delegated to a service even if the service account is set as trusted for Kerberos delegation. 
-      NOT_DELEGATED = 1048576
+    # The security context of the user will not be delegated to a service even if the service account is set as trusted for Kerberos delegation. 
+    NOT_DELEGATED = 1048576
 
-      # Restrict this principal to use only Data Encryption Standard (DES) encryption types for keys. 
-      USE_DES_KEY_ONLY = 2097152
+    # Restrict this principal to use only Data Encryption Standard (DES) encryption types for keys. 
+    USE_DES_KEY_ONLY = 2097152
 
-      # This account does not require Kerberos pre-authentication for logon. 
-      DONT_REQUIRE_PREAUTH = 4194304
+    # This account does not require Kerberos pre-authentication for logon. 
+    DONT_REQUIRE_PREAUTH = 4194304
 
-      # The user password has expired. This flag is created by the system using data from the Pwd-Last-Set attribute and the domain policy. 
-      PASSWORD_EXPIRED = 8388608
+    # The user password has expired. This flag is created by the system using data from the Pwd-Last-Set attribute and the domain policy. 
+    PASSWORD_EXPIRED = 8388608
 
-      # The account is enabled for delegation. This is a security-sensitive setting; accounts with this option enabled should be strictly 
-      # controlled. This setting enables a service running under the account to assume a client identity and authenticate as that user to 
-      # other remote servers on the network.
-      TRUSTED_TO_AUTHENTICATE_FOR_DELEGATION = 16777216
+    # The account is enabled for delegation. This is a security-sensitive setting; accounts with this option enabled should be strictly 
+    # controlled. This setting enables a service running under the account to assume a client identity and authenticate as that user to 
+    # other remote servers on the network.
+    TRUSTED_TO_AUTHENTICATE_FOR_DELEGATION = 16777216
 
-      PARTIAL_SECRETS_ACCOUNT = 67108864
+    PARTIAL_SECRETS_ACCOUNT = 67108864
 
-      USE_AES_KEYS = 134217728
-   }
+    USE_AES_KEYS = 134217728
+}
+
+  
