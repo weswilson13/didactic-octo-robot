ec90bbf186d570613ad6120d52abf7e315ca7838
weswilson13 <102468132+weswilson13@users.noreply.github.com>
2025-11-14 16:17:37 -0500

    Refactor file version retrieval and logging

--- PrimeNet/Invoke-MonitorFileTransfers.ps1
+++ PrimeNet/Invoke-MonitorFileTransfers.ps1
@@ -9,7 +9,13 @@ function Import-FileTransferCSV {
 
     foreach ($obj in $data) {
         #$versionInfo = $obj.VersionInfo.Split("`n").foreach({$key,$value=$_.split(':',2); @{$key.trim()=$value.trim()}})
-        $null=Add-Member -InputObject $obj -NotePropertyMembers @{VersionInfo = $obj.VersionInfo.Split("`n").foreach({$key,$value=$_.split(':',2); @{$key.trim()=$value.trim()}})} -Force -PassThru
+        $notePropertyMembers = @{
+            VersionInfo = $obj.VersionInfo.Split("`n").foreach({
+                $key,$value=$_.split(':',2)
+                [psobject]@{$key.trim()=$value.trim()}
+            })
+        }
+        $null=Add-Member -InputObject $obj -NotePropertyMembers $notePropertyMembers -Force -PassThru
         $result += $obj
     }
 
@@ -19,20 +25,50 @@ function Import-FileTransferCSV {
 }
 
 function Get-FileVersion {
+<#
+    .SYNOPSIS
+    Returns string containing the file version, derived preferentially from the object VersionInfo.
+
+    We first check the FileVersion attribute. If this value is empty, then check the ProductVersion attribute. Next check filename for a valid version.
+    If still no version returned, use AppLocker to try to pull version info - this method requires the file to be present.
+#>
+
     [cmdletbinding()]
     param([object]$FileInfo)
 
-    if ($FileInfo.VersionInfo.FileVersion) {
+    if ($FileInfo.VersionInfo.FileVersion -and $FileInfo.BaseName -notmatch 'idrac|bios') { # DELL seems to store the actual version in the ProductVersion attribute
         $newVersion = $FileInfo.VersionInfo.FileVersion
+
+        if ([version]::TryParse($newVersion,[ref]$null)) {
+            return $newVersion
+        }
     }
-    elseif ($FileInfo.VersionInfo.ProductVersion) { # check ProductVersion property
+    
+    if ($FileInfo.VersionInfo.ProductVersion) { # check ProductVersion property
         $newVersion = $FileInfo.VersionInfo.ProductVersion
-    }
+
+        if ([version]::TryParse($newVersion,[ref]$null)) {
+            return $newVersion
+        }
+    } 
     else { # try to get a version from the file name
         $newVersion = [regex]::Match($FileInfo.BaseName, '(\d+\.?)+').Value
+
+        if ([version]::TryParse($newVersion,[ref]$null)) {
+            return $newVersion
+        }
     }
 
-    return $newVersion
+    if (Test-Path $FileInfo.FullName) { # leverage applocker to try to pull out the version (SLOW!!!!)
+        #Write-Host "Collecting AppLocker file information"
+        $newVersion = (Get-AppLockerFileInformation -Path $FileInfo.FullName).Publisher.BinaryVersion.ToString()
+        
+        if ([version]::TryParse($newVersion,[ref]$null)) {
+            return $newVersion
+        }
+    }
+
+    throw "Unable to determine a valid file version"
 }
 
 $log = "$env:USERPROFILE\OneDrive\OneDrive - PrimeNet\SoftwareVersions\FileTransferLog.csv"
@@ -43,44 +79,73 @@ while ($true) { # loop indefinitely
     $files = Get-ChildItem "$env:USERPROFILE\OneDrive\OneDrive - PrimeNet\ToNNPP" 
 
     foreach ($file in $files) { # check each file against the log
-        Write-Host "New file detected - $($file.Name)"
+        Write-Host "New file detected - $($file.Name.TrimStart('_'))"
 
-        if ($file.Name -in $transferredFiles.Name) { # if the file already exists in the log, skip it
+        # wait for the file to finish downloading
+        Write-Host "`tWaiting for download to complete."
+        $lastSize = 0
+        $waiting = "`t."
+        # Loop until the file size remains unchanged for 10 seconds
+
+        do {
+            while ($true) {
+                Start-Sleep -Seconds 5
+                $newSize = Get-ItemPropertyValue -Path $file.FullName -Name Length
+               # ('"LastSize","NewSize"
+               # "{0}","{1}"' -f $lastSize,$newSize) | ConvertFrom-Csv | format-table
+
+                if ($newSize -eq $lastSize) {
+                    Write-Host "`tFile size is stable. Download complete."
+                    break
+                }
+                $lastSize = $newSize
+            
+                $waiting += "."
+                Write-Host $waiting
+            }
+
+            try { 
+                $_file = Rename-Item -Path $file.FullName -NewName $file.Name.TrimStart('_') -PassThru -ErrorAction Stop
+                $file = $_file
+            }
+            catch {
+                Write-Host "`t$($error[0].Exception.message)" -ForegroundColor Yellow
+            }
+        } while ($file.Name.StartsWith('_'))
+
+        Write-Host "`t$($file.Name) has finished downloading"
+
+        if ($logEntry = $transferredFiles | Where-Object {$_.Name -eq $file.Name}) { # if the file already exists in the log, skip it
             Write-Host "`tFound a matching filename in the log. Comparing file versions..." -NoNewline
-            $logEntry = $transferredFiles | Where-Object {$_.Name -eq $file.Name}
-            [string]$fileVersion = $file.VersionInfo.FileVersion
-            $logVersion = $logEntry.VersionInfo.FileVersion.Trim()
 
-            if ($fileVersion -in $logVersion) { 
+            try {
+                $fileVersion = ""
+                [string]$fileVersion = Get-FileVersion (Get-Item $file.FullName)
+                $fileVersion = $fileVersion.Trim()
+            }
+            catch {
+                Write-Host "`t$($error[0].Exception.message)" -ForegroundColor Red
+            }
+
+            $logVersion = $logEntry._FileVersion
+
+            #'"File Version","Log Version"
+            # "{0}","{1}"' -f $fileVersion, (($logVersion | Sort-Object -Descending) -join ',') | ConvertFrom-Csv | Out-String | Write-Host -ForegroundColor Cyan
+              
+            if ($fileVersion.Trim() -in $logVersion) { 
                 Write-Host "MATCH FOUND" -ForegroundColor Yellow
-                Write-Host "`t$($file.Name) @ $($file.VersionInfo.FileVersion) is already in the log."
+                Write-Host "`t$($file.Name) @ $($fileVersion) is already in the log."
+                Write-Host ""
                 continue 
             }
             Write-Host "NO MATCH" -ForegroundColor DarkGreen
         }
 
-        # wait for the file to finish downloading
-        $lastSize = 0
-
-        # Loop until the file size remains unchanged for 10 seconds
-        while ($true) {
-            Start-Sleep -Seconds 5
-            $newSize = (Get-Item $file.FullName).Length
-            if ($newSize -eq $lastSize) {
-                Write-Host "`tFile size is stable. Download complete."
-                break
-            }
-            $lastSize = $newSize
-        }
-
-        Write-Host "`t$($file.Name) has finished downloading"
-
         # add file to log
-        if ([string]$file.VersionInfo.FileVersion -eq "") { # try to add the file version using our function
-            $file.VersionInfo.FileVersion = Get-FileVersion $file
-        }
+        $null=Add-Member -InputObject $file -NotePropertyName '_FileVersion' -NotePropertyValue $fileVersion.Trim() -Force -PassThru
         $file | Export-Csv $log -NoTypeInformation -Append 
-        Write-Host "`tAdded $($file.Name) to log."
+        Write-Host "`tAdded $($file.Name) to log." -ForegroundColor Green
+        Write-Host ""
     }
 
     # wait 30 seconds between polls
