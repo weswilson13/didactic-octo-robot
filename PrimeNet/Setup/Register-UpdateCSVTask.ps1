5c12f2d62c6997d26ce5ea8b945dbb488c97c773
weswilson13 <102468132+weswilson13@users.noreply.github.com>
2025-11-30 19:32:30 -0500

    Create Register-UpdateCSVTask.ps1

---
+++ PrimeNet/Setup/Register-UpdateCSVTask.ps1
@@ -0,0 +1,29 @@
+function Get-RegistryValue {
+    [cmdletbinding()]
+    param(
+        [ValidateSet('MonitoredFolder','TransferLog','SoftwareVersions','ActivityLog')]
+        [string]$Key
+    )
+
+    try { 
+        $value = Get-ItemProperty HKCU:\Environment\FileTransfer -Name $Key -ErrorAction Stop | Select-Object -ExpandProperty $Key
+    }
+    catch {
+        throw "$($error[0].Exception.Message). Please run Set-RegistryKeys.ps1 in SoftwareVersions\Setup to configure the necessary registry settings."
+    }
+
+    return $value
+}
+
+$path = Join-Path (Get-RegistryValue SoftwareVersions) ScheduledTask\XML
+
+$xmlRaw = Get-Content $path\UpdateSoftwareStatusCSV_Headless.xml -Raw
+$xmlRaw = $xmLRaw -replace '%USERNAME%', $env:USERNAME
+
+$guid = [guid]::NewGuid().Guid
+$tempPath = "$env:TEMP\${guid}.xml"
+$xmlRaw | Out-File $tempPath
+
+schtasks.exe /Create /XML $tempPath /tn UpdateSoftwareStatusCSV
+
+Remove-Item $tempPath -Force
